a9d63293c16a33d4b7f967b43bfcc5127e8336e5
shawncodes <shawn@huthunt.com>
2018-07-25 23:13:18 -0400

    fixed circular dependency issue

--- app/controllers/api/v1/inputs_controller.rb
+++ app/controllers/api/v1/inputs_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::InputsController < ApplicationController
+  before_action :find_input, only: [:update]
+  def index
+    @inputs = Input.all
+    render json: @inputs
+  end
+
+  def update
+    @input.update(input_params)
+    if @input.save
+      render json: @input, status: :accepted
+    else
+      render json: { errors: @input.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def input_params
+    params.permit(:title, :content)
+  end
+
+  def find_input
+    @input = Input.find(params[:id])
+  end
 end
